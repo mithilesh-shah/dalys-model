f4f4a16b37fff39679c238253fdc83a74017e08c
julianashwin <julianashwin@gmail.com>
2023-08-03 10:04:24 +0200

    redo incidence charts

--- basic_data_cleaning.R
+++ basic_data_cleaning.R
@@ -21,18 +21,22 @@ for (file in GBD_files){
     mutate(measure_name = str_replace(measure_name, "YLLs \\(Years of Life Lost\\)", "YLLs")) %>%
     mutate(measure_name = str_replace(measure_name, "DALYs \\(Disability-Adjusted Life Years\\)", "DALYs")) %>%
     mutate(age_name = str_replace(age_name, "<1 year", "0-1 years")) %>%
-    mutate(age_name = str_replace(age_name, "95\\+ years", "95-99 years"))
+    mutate(age_name = str_replace(age_name, "95\\+ years", "95-99 years")) %>%
+    mutate(age_name = case_when(str_detect(age_name, "years") ~ age_name, TRUE ~ str_c(age_name, " years")))
+    
   # Append
   gbd_df_all <- rbind(gbd_df_all, df_in)
 }
 gbd_df_all %>%
+  mutate(age_name = case_when(str_detect(age_name, "years") ~ age_name, TRUE ~ str_c(age_name, " years"))) %>%
   arrange(location_name, cause_name, measure_name, metric_name, age_name, sex_name) %>%
   saveRDS("/Users/julianashwin/Documents/Research/DALYs/data/gbd_data_all.rds")
+#gbd_df_all <- readRDS("/Users/julianashwin/Documents/Research/DALYs/data/gbd_data_all.rds")
+
 # Save names of all countries in GBD data
 gbd_df_all %>%
   distinct(location_name) %>%
   saveRDS("raw_data/GBD/gbd_data_countries.rds")
-
 # Save deaths data
 gbd_df_all %>%
   filter(measure_name == "Deaths") %>%
@@ -50,7 +54,7 @@ gbd_df_all %>%
   saveRDS("raw_data/GBD/gbd_data_ylls.rds")
 # Save DALYs data
 gbd_df_all %>%
-  filter(measure_name == "DALYS") %>%
+  filter(measure_name == "DALYs") %>%
   filter(sex_name == "Both") %>%
   saveRDS("raw_data/GBD/gbd_data_dalys.rds")
 # Save Incidence data
@@ -66,6 +70,15 @@ gbd_countries <- readRDS("raw_data/GBD/gbd_data_countries.rds")
 "
 Clean cluster membership data
 "
+# Import hierarchy of causes
+gbd_hierarchy_df <- read_xlsx("raw_data/GBD/IHME_GBD_2019_HIERARCHIES.xlsx", sheet = "Cause Hierarchy") %>%  
+  rename_all(~tolower(str_replace_all(., "\\s+", "_"))) %>%
+  right_join(distinct(gbd_df_all, cause_name)) %>%
+  separate(cause_outline, into = c("level1_outline",  "level2_outline", "level3_outline", "level4_outline"), remove = FALSE) %>%
+  mutate(level2_outline = str_c(level1_outline, ".", level2_outline),
+         level3_outline = str_c(level2_outline, ".", level3_outline),
+         level4_outline = cause_outline)
+# Clusters with K = 3
 cluster3_df <- read_xlsx("raw_data/clusters/kmeans_clusters_3.xlsx") %>%
   select(-`...1`) %>%
   filter(!str_detect(cause_name, "Total")) %>%
@@ -77,7 +90,7 @@ cluster3_df <- read_xlsx("raw_data/clusters/kmeans_clusters_3.xlsx") %>%
                              cluster_no == 1  ~ "Adult",
                              cluster_no == 2  ~ "Infant")) %>%
   dplyr::select(cause_name, age, age_name, incidence, cluster_3)
-
+# Clusters with K = 4
 cluster4_df <- read_xlsx("raw_data/clusters/kmeans_clusters_4.xlsx") %>%
   select(-`...1`) %>%
   filter(!str_detect(cause_name, "Total")) %>%
@@ -90,10 +103,32 @@ cluster4_df <- read_xlsx("raw_data/clusters/kmeans_clusters_4.xlsx") %>%
                              cluster_no == 2  ~ "Infant",
                              cluster_no == 3  ~ "Age-related")) %>%
   dplyr::select(cause_name, age, cluster_4)
-
+# Merge clusters
 clusters_df <- cluster3_df %>%
-  left_join(cluster4_df)
-
+  left_join(cluster4_df) %>%
+  left_join(select(gbd_hierarchy_df, cause_name, cause_outline))
+# Plot incidence for K = 3
+clusters_df %>%
+  ggplot(aes(x = age, y = incidence)) + theme_bw() + 
+  geom_line(aes(group = cause_name, color = cluster_3), alpha = 0.2, size = 0.2) +
+  geom_line(aes(color = cluster_3), stat = "summary", fun = "mean", size = 0.5) + 
+  scale_color_manual("Disease clusters", values = c("Age-related" = "firebrick1", "Adult" = "blue3", "Infant" = "forestgreen")) + 
+  xlab("Age") + ylab("Incidence (standardised)") + 
+  scale_y_continuous(limits = c(-2.5,4.5), expand = c(0, 0)) +
+  theme(axis.ticks.y = element_blank(), axis.text.y = element_blank())
+ggsave("figures/clustering/clusters3.pdf", width = 6, height = 3)
+# Plot incidence for K = 4
+clusters_df %>%
+  ggplot(aes(x = age, y = incidence)) + theme_bw() + 
+  geom_line(aes(group = cause_name, color = cluster_4), alpha = 0.2, size = 0.2) +
+  geom_line(aes(color = cluster_4), stat = "summary", fun = "mean", size = 0.5) + 
+  scale_color_manual("Disease cluster", values = c("Age-related" = "firebrick1", "Adult (late)" = "blue3", 
+                                                   "Adult (early)" = "cornflowerblue", "Infant" = "forestgreen")) + 
+  xlab("Age") + ylab("Incidence (standardised)") + 
+  scale_y_continuous(limits = c(-2.5,4.5), expand = c(0, 0)) +
+  theme(axis.ticks.y = element_blank(), axis.text.y = element_blank())
+ggsave("figures/clustering/clusters4.pdf", width = 6, height = 3)
+# Save 
 clusters_df %>%
   write_csv("clean_data/cluster_membership_data.csv")
 
@@ -244,10 +279,14 @@ pop_df %>%
   left_join(lifetab_df) %>%
   ggplot() + theme_bw() + facet_wrap(~year) +
   geom_line(aes(x = age, y = survival, group = location_name, alpha = population))
-  
+
 pop_df %>%
   left_join(lifetab_df) %>%
+  mutate(age_name = str_c(plyr::round_any(age,5, f = floor), "-", plyr::round_any(age,5, f = floor)+4, " years"),
+         age_name = case_when(age == 0 ~ "0-1 years", age >=1 & age < 5 ~  "1-4 years", 
+                              age >= 100 ~ "100+ years", TRUE ~ age_name)) %>%
   write_csv("clean_data/country_lifetab_data.csv")
+  
 
 
 "
@@ -256,98 +295,98 @@ WPP Fertility rates
 ## Fertility estimates
 fertility_est_df <- read_xlsx("raw_data/WPP/WPP2022_FERTILITY_RATES_BY_SINGLE_AGE_OF_MOTHER.xlsx",
                               sheet = "Estimates", skip = 16) %>%
-  rename(location = `Region, subregion, country or area *`, 
+  rename(location_name = `Region, subregion, country or area *`, 
          type = Type, year = Year) %>%
   filter(year >= 2019, type == "Country/Area") %>%
-  dplyr::select(c(location, type, year, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`,
+  dplyr::select(c(location_name, type, year, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`,
            `26`, `27`, `28`, `29`, `30`, `31`, `32`, `33`, `34`, `35`, `36`, `37`, `38`, `39`,
            `40`, `41`, `42`, `43`, `44`, `45`, `46`, `47`, `48`, `49`)) %>%
-  mutate_at(vars(-location, -type), as.numeric) %>%
-  pivot_longer(cols = -c(location, type, year), names_to = "age", values_to = "fertility") %>%
+  mutate_at(vars(-location_name, -type), as.numeric) %>%
+  pivot_longer(cols = -c(location_name, type, year), names_to = "age", values_to = "fertility") %>%
   mutate(fertility = fertility/2000, age = as.numeric(age)) %>% filter(!is.na(fertility)) %>%
   rename(fertility_est = fertility) %>%
-  mutate(location = case_when(location == "State of Palestine" ~ "Palestine", 
-                              location == "Türkiye" ~ "Turkey", 
-                              location == "China, Taiwan Province of China" ~ "Taiwan (Province of China)", 
-                              location == "Dem. People's Republic of Korea" ~ "Democratic People's Republic of Korea",
-                              location == "Micronesia (Fed. States of)" ~ "Micronesia (Federated States of)",
-                              TRUE ~ location)) %>%
-  filter(location %in% gbd_countries$location_name) %>%
-  full_join(crossing(year = 2021:2100, age = 15:49, location = gbd_countries$location_name),
-            by = c("location", "year", "age")) %>%
-  select(-type) %>% arrange(location, age, year) %>%
+  mutate(location_name = case_when(location_name == "State of Palestine" ~ "Palestine", 
+                                   location_name == "Türkiye" ~ "Turkey", 
+                                   location_name == "China, Taiwan Province of China" ~ "Taiwan (Province of China)", 
+                                   location_name == "Dem. People's Republic of Korea" ~ "Democratic People's Republic of Korea",
+                                   location_name == "Micronesia (Fed. States of)" ~ "Micronesia (Federated States of)",
+                              TRUE ~ location_name)) %>%
+  filter(location_name %in% gbd_countries$location_name) %>%
+  full_join(crossing(year = 2021:2100, age = 15:49, location_name = gbd_countries$location_name),
+            by = c("location_name", "year", "age")) %>%
+  select(-type) %>% arrange(location_name, age, year) %>%
   fill(fertility_est, .direction = "down") %>%
-  arrange(location, year, age)
+  arrange(location_name, year, age)
 ## Low fertility projections
 fertility_low_df <- as.data.frame(read_xlsx("raw_data/WPP/WPP2022_FERTILITY_RATES_BY_SINGLE_AGE_OF_MOTHER.xlsx",
                                             sheet = "Low variant", skip = 16)) %>%
-  rename(location = `Region, subregion, country or area *`, 
+  rename(location_name = `Region, subregion, country or area *`, 
          type = Type, year = Year) %>% 
   filter(year >= 2019, type == "Country/Area") %>%
-  select(c(location, type, year, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`,
+  select(c(location_name, type, year, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`,
            `26`, `27`, `28`, `29`, `30`, `31`, `32`, `33`, `34`, `35`, `36`, `37`, `38`, `39`,
            `40`, `41`, `42`, `43`, `44`, `45`, `46`, `47`, `48`, `49`)) %>%
-  mutate_at(vars(-location, -type), as.numeric) %>%
-  pivot_longer(cols = -c(location, type, year), names_to = "age", values_to = "fertility") %>%
+  mutate_at(vars(-location_name, -type), as.numeric) %>%
+  pivot_longer(cols = -c(location_name, type, year), names_to = "age", values_to = "fertility") %>%
   mutate(fertility = fertility/2000, age = as.numeric(age)) %>% filter(!is.na(fertility)) %>%
   rename(fertility_low = fertility) %>%
-  mutate(location = case_when(location == "State of Palestine" ~ "Palestine", 
-                              location == "Türkiye" ~ "Turkey", 
-                              location == "China, Taiwan Province of China" ~ "Taiwan (Province of China)", 
-                              location == "Dem. People's Republic of Korea" ~ "Democratic People's Republic of Korea",
-                              location == "Micronesia (Fed. States of)" ~ "Micronesia (Federated States of)",
-                              TRUE ~ location)) %>%
-  filter(location %in% gbd_countries$location_name) %>%
-  select(-type) %>% arrange(location, year, age)
+  mutate(location_name = case_when(location_name == "State of Palestine" ~ "Palestine", 
+                              location_name == "Türkiye" ~ "Turkey", 
+                              location_name == "China, Taiwan Province of China" ~ "Taiwan (Province of China)", 
+                              location_name == "Dem. People's Republic of Korea" ~ "Democratic People's Republic of Korea",
+                              location_name == "Micronesia (Fed. States of)" ~ "Micronesia (Federated States of)",
+                              TRUE ~ location_name)) %>%
+  filter(location_name %in% gbd_countries$location_name) %>%
+  select(-type) %>% arrange(location_name, year, age)
 
 ## Medium fertility projections
 fertility_med_df <- as.data.frame(read_xlsx("raw_data/WPP/WPP2022_FERTILITY_RATES_BY_SINGLE_AGE_OF_MOTHER.xlsx",
                                             sheet = "Medium variant", skip = 16)) %>%
-  rename(location = `Region, subregion, country or area *`, 
+  rename(location_name = `Region, subregion, country or area *`, 
          type = Type, year = Year) %>% 
   filter(year >= 2019, type == "Country/Area") %>%
-  select(c(location, type, year, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`,
+  select(c(location_name, type, year, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`,
            `26`, `27`, `28`, `29`, `30`, `31`, `32`, `33`, `34`, `35`, `36`, `37`, `38`, `39`,
            `40`, `41`, `42`, `43`, `44`, `45`, `46`, `47`, `48`, `49`)) %>%
-  mutate_at(vars(-location, -type), as.numeric) %>%
-  pivot_longer(cols = -c(location, type, year), names_to = "age", values_to = "fertility") %>%
+  mutate_at(vars(-location_name, -type), as.numeric) %>%
+  pivot_longer(cols = -c(location_name, type, year), names_to = "age", values_to = "fertility") %>%
   mutate(fertility = fertility/2000, age = as.numeric(age)) %>% filter(!is.na(fertility)) %>%
   rename(fertility_med = fertility) %>%
-  mutate(location = case_when(location == "State of Palestine" ~ "Palestine", 
-                              location == "Türkiye" ~ "Turkey", 
-                              location == "China, Taiwan Province of China" ~ "Taiwan (Province of China)", 
-                              location == "Dem. People's Republic of Korea" ~ "Democratic People's Republic of Korea",
-                              location == "Micronesia (Fed. States of)" ~ "Micronesia (Federated States of)",
-                              TRUE ~ location)) %>%
-  filter(location %in% gbd_countries$location_name) %>%
-  select(-type) %>% arrange(location, year, age)
+  mutate(location_name = case_when(location_name == "State of Palestine" ~ "Palestine", 
+                              location_name == "Türkiye" ~ "Turkey", 
+                              location_name == "China, Taiwan Province of China" ~ "Taiwan (Province of China)", 
+                              location_name == "Dem. People's Republic of Korea" ~ "Democratic People's Republic of Korea",
+                              location_name == "Micronesia (Fed. States of)" ~ "Micronesia (Federated States of)",
+                              TRUE ~ location_name)) %>%
+  filter(location_name %in% gbd_countries$location_name) %>%
+  select(-type) %>% arrange(location_name, year, age)
 ## High fertility projections
 fertility_high_df <- as.data.frame(read_xlsx("raw_data/WPP/WPP2022_FERTILITY_RATES_BY_SINGLE_AGE_OF_MOTHER.xlsx",
                                              sheet = "High variant", skip = 16)) %>%
-  rename(location = `Region, subregion, country or area *`, 
+  rename(location_name = `Region, subregion, country or area *`, 
          type = Type, year = Year) %>% 
   filter(year >= 2019, type == "Country/Area") %>%
-  select(c(location, type, year, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`,
+  select(c(location_name, type, year, `15`, `16`, `17`, `18`, `19`, `20`, `21`, `22`, `23`, `24`, `25`,
            `26`, `27`, `28`, `29`, `30`, `31`, `32`, `33`, `34`, `35`, `36`, `37`, `38`, `39`,
            `40`, `41`, `42`, `43`, `44`, `45`, `46`, `47`, `48`, `49`)) %>%
-  mutate_at(vars(-location, -type), as.numeric) %>%
-  pivot_longer(cols = -c(location, type, year), names_to = "age", values_to = "fertility") %>%
+  mutate_at(vars(-location_name, -type), as.numeric) %>%
+  pivot_longer(cols = -c(location_name, type, year), names_to = "age", values_to = "fertility") %>%
   mutate(fertility = fertility/2000, age = as.numeric(age)) %>% filter(!is.na(fertility)) %>%
   rename(fertility_high = fertility) %>%
-  mutate(location = case_when(location == "State of Palestine" ~ "Palestine", 
-                              location == "Türkiye" ~ "Turkey", 
-                              location == "China, Taiwan Province of China" ~ "Taiwan (Province of China)", 
-                              location == "Dem. People's Republic of Korea" ~ "Democratic People's Republic of Korea",
-                              location == "Micronesia (Fed. States of)" ~ "Micronesia (Federated States of)",
-                              TRUE ~ location)) %>%
-  filter(location %in% gbd_countries$location_name) %>%
-  select(-type) %>% arrange(location, year, age)
+  mutate(location_name = case_when(location_name == "State of Palestine" ~ "Palestine", 
+                              location_name == "Türkiye" ~ "Turkey", 
+                              location_name == "China, Taiwan Province of China" ~ "Taiwan (Province of China)", 
+                              location_name == "Dem. People's Republic of Korea" ~ "Democratic People's Republic of Korea",
+                              location_name == "Micronesia (Fed. States of)" ~ "Micronesia (Federated States of)",
+                              TRUE ~ location_name)) %>%
+  filter(location_name %in% gbd_countries$location_name) %>%
+  select(-type) %>% arrange(location_name, year, age)
 ## Merge fertility together
 fertility_df <- fertility_est_df %>%
   full_join(fertility_low_df) %>%
   full_join(fertility_med_df) %>%
   full_join(fertility_high_df) %>%
-  arrange(location, year, age) %>%
+  arrange(location_name, year, age) %>%
   mutate(fertility_low = case_when(year <= 2021 ~ fertility_est, year > 2021 ~ fertility_low)) %>%
   mutate(fertility_med = case_when(year <= 2021 ~ fertility_est, year > 2021 ~ fertility_med)) %>%
   mutate(fertility_high = case_when(year <= 2021 ~ fertility_est, year > 2021 ~ fertility_high))
--- static_analysis.R
+++ static_analysis.R
@@ -8,105 +8,96 @@ library(janitor)
 
 
 "
+Define age factor
+"
+age_groups <- c("0-1 years", "1-4 years", "5-9 years", "10-14 years", "15-19 years", "20-24 years", "25-29 years", 
+                "30-34 years", "35-39 years", "40-44 years", "45-49 years", "50-54 years", "55-59 years",
+                "60-64 years", "65-69 years", "70-74 years", "75-79 years", "80-84 years ", "85-89 year",  
+                "90-94 years", "95-99 years")
+
+"
 Import data
 "
 cluster_df <- read_csv("clean_data/cluster_membership_data.csv")
 macro_df <- read_csv("clean_data/country_wdi_data.csv")
 lifetab_df <- read_csv("clean_data/country_lifetab_data.csv")
 gbd_deaths_df <- readRDS("raw_data/GBD/gbd_data_deaths.rds")
+gbd_ylds_df <- readRDS("raw_data/GBD/gbd_data_ylds.rds")
+gbd_dalys_df <- readRDS("raw_data/GBD/gbd_data_dalys.rds")
 gbd_ylls_df <- readRDS("raw_data/GBD/gbd_data_ylls.rds")
 
+## Aggregate up lifetable to group level
+lifetab_grp_df <- lifetab_df %>%
+  arrange(location_name, year, age) %>%
+  group_by(location_name, year, age_name) %>%
+  summarise(age_mid = sum(population*age)/sum(population), 
+            mortality = sum(population*mortality)/sum(population),
+            survival = sum(population*survival)/sum(population),
+            remaining_le = sum(population*remaining_le)/sum(population),
+            population = sum(population)) %>%
+  arrange(location_name, year, age_mid)
 
-
+## Latest available year from macro data
+macro_latest_df <- macro_df %>% na.omit() %>%
+  arrange(location_name, year) %>%
+  group_by(location_name) %>%
+  summarise_all(last)
 
 
 "
-Plot cluster membership
+What actually is a YLD?
 "
+gbd_ylds_df %>%
+  filter(metric_name == "Rate") %>%
+  group_by(location_name, age_name) %>%
+  summarise(lower = sum(lower/1e5), val = sum(val/1e5), upper = sum(upper/1e5)) %>%
+  tabyl(age_name)
+  mutate(age = factor(str_remove(age_name, " years"), ordered = T, levels = str_remove(age_groups, " years"))) %>%
+  ggplot() + theme_bw() + 
+  geom_violin(aes(x = age, y = val))
+  
+  
+"
+Decompose mortality and disability by cluster
+"  
+gbd_deaths_df %>%
+  left_join(lifetab_grp_df)
+filter(measure_name == "Deaths" & metric_name == "Rate") %>%
+  select(-measure_name, -upper, -lower) %>%
+  pivot_wider(id_cols = c(year, location_name, sex_name, age_name, age_low), names_from = cause_name, 
+              values_from = val, values_fill = 0) %>%
+  arrange(year, location_name, age_low) %>% rowwise() %>%
+  mutate(mortality = sum(c_across(`HIV/AIDS resulting in other diseases`:`Alcoholic cardiomyopathy`))) %>%
+  relocate(mortality, .after = age_low) %>%
+  full_join(fill_df, by = c("year", "location_name","age_low")) %>% ungroup() %>%
+  arrange(year, location_name, age_low) %>% 
+  rename(age = age_low, location = location_name) %>%
+  fill(!age_name, .direction = "down")
 
-cluster_df %>%
-  mutate(cluster_4 = factor(cluster_4, ordered = T, levels = c("Infant", "Adult (early)", "Adult (late)", "Age-related"))) %>% 
-  ggplot(aes(x = age, y = incidence)) + theme_bw() + 
-  geom_line(aes(group = cause_name, color = cluster_4), alpha = 0.2, size = 0.2) +
-  geom_line(aes(color = cluster_4), stat = "summary", fun = "mean", size = 0.5) + 
-  scale_color_manual("Cluster", values =  c("Age-related" = "firebrick1", "Adult (late)" = "blue3", 
-                                            "Adult (early)" = "cornflowerblue", "Infant" = "forestgreen")) + 
-  xlab("Age") + ylab("Incidence (standardised)") + 
-  scale_y_continuous(limits = c(-2.5,4.5), expand = c(0, 0)) +
-  theme(axis.ticks.y = element_blank(), axis.text.y = element_blank())
-ggsave("figures/clusters4.pdf", width = 6, height = 3)
+  
 
+max(total_yld_df$val)
 
-
-df_old <- read_csv("raw_data/YLD_YLL_2019_countries.csv") %>%
+df_old <- gbd_ylls_df %>%
   filter(metric_name == "Number") %>%
-  filter(location_name != "Taiwan (Province of China)", location_name != "Venezuela (Bolivarian Republic of)",
-         location_name != "Palestine", location_name != "Cook Islands", location_name != "Niue", 
-         location_name != "Tokelau") %>%
-  filter(cause_name != "Total cancers")
+  inner_join(select(cluster_df, cause_name, cluster_4)) %>%
+  inner_join(select(macro_latest_df, location_name, WDI_population, income_group)) %>%
+  group_by(location_name, income_group, WDI_population, cluster_4, measure_name) %>%
+  summarise(val = sum(val), upper = sum(upper), lower = sum(lower)) %>%
+  pivot_wider(id_cols = c(location_name, WDI_population, income_group, cluster_4), 
+              names_from = measure_name, values_from = c(val)) 
 
+df_old <- gbd_deaths_df %>%
+  filter(metric_name == "Number") %>%
+  left_join(lifetab_grp_df)
+  
 
-classification_data <- read_xlsx("raw_data/WDIEXCEL.xlsx", sheet = "Country") %>%
-  filter(!is.na(`Income Group`)) %>%
-  select(`Country Code`, `Table Name`, `Income Group`) %>%
-  rename(name = `Table Name`, code = `Country Code`, income_group = `Income Group`) %>%
-  mutate(income_group = case_when(income_group == "High income" ~ "World Bank High Income",
-                                  income_group == "Upper middle income" ~ "World Bank Upper Middle Income",
-                                  income_group == "Lower middle income" ~ "World Bank Lower Middle Income",
-                                  income_group == "Low income" ~ "World Bank Low Income")) %>%
-  mutate(location_name = case_when(name == "Korea, Dem. People's Rep." ~ "Democratic People's Republic of Korea",
-                                   name == "Micronesia, Fed. Sts." ~ "Micronesia (Federated States of)",
-                                   name == "Lao PDR" ~ "Lao People's Democratic Republic",
-                                   name == "Vietnam" ~ "Viet Nam",
-                                   name == "Kyrgyz Republic" ~ "Kyrgyzstan",
-                                   name == "Slovak Republic" ~ "Slovakia",
-                                   name == "Moldova" ~ "Republic of Moldova",
-                                   name == "Korea, Rep." ~ "Republic of Korea",
-                                   name == "United States" ~ "United States of America",
-                                   name == "Bahamas, The" ~ "Bahamas",
-                                   name == "St. Lucia" ~ "Saint Lucia", 
-                                   name == "St. Vincent and the Grenadines" ~ "Saint Vincent and the Grenadines", 
-                                   name == "Bolivia" ~ "Bolivia (Plurinational State of)", 
-                                   name == "Egypt, Arab Rep." ~ "Egypt", 
-                                   name == "Iran, Islamic Rep." ~ "Iran (Islamic Republic of)", 
-                                   name == "Türkiye" ~ "Turkey", 
-                                   name == "Yemen, Rep." ~ "Yemen", 
-                                   name == "Congo, Rep." ~ "Congo", 
-                                   name == "Congo, Dem. Rep." ~ "Democratic Republic of the Congo", 
-                                   name == "Tanzania" ~ "United Republic of Tanzania", 
-                                   name == "Gambia, The" ~ "Gambia", 
-                                   name == "São Tomé and Principe" ~ "Sao Tome and Principe", 
-                                   name == "St. Kitts and Nevis" ~ "Saint Kitts and Nevis", 
-                                   name == "Virgin Islands (U.S.)" ~ "United States Virgin Islands",
-                                   TRUE ~ name))
-country_codes <- unique(classification_data$code)
-
-pop_df_in <- read_xlsx("raw_data/WDIEXCEL.xlsx", sheet = "Data")
-
-pop_df <- pop_df_in %>%
-  filter(`Indicator Code` %in% c("SP.POP.TOTL")) %>%
-  select(-`Indicator Name`) %>%
-  rename(name = `Country Name`, code = `Country Code`, ind_code = `Indicator Code`) %>%
-  pivot_longer(cols = -c(name, code, ind_code), names_to = "year", values_to = "val") %>% 
-  mutate(series = case_when(ind_code == "SP.POP.TOTL"  ~ "population")) %>%
-  pivot_wider(id_cols = c(name, code, year), names_from = series, values_from = val) %>%
-  mutate(year = as.numeric(year)) %>%
-  mutate(location = case_when(name == "World"  ~ "Global",
-                              name == "High income" ~ "World Bank High Income",
-                              name == "Upper middle income" ~ "World Bank Upper Middle Income",
-                              name == "Lower middle income" ~ "World Bank Lower Middle Income",
-                              name == "Low income" ~ "World Bank Low Income",
-                              code %in% country_codes ~ "Country",
-                              TRUE ~ "Drop")) %>%
-  filter(location == "Country" & year == 2019) %>% 
-  select(code, year, population)
-
-unique(df$location_name)[!(unique(df$location_name) %in% classification_data$location_name)]
+gbd_deaths_df %>%
 
 
 
-df_out <- df %>%
-  inner_join(cluster4_df) %>%
+df_out <- gbd_ylls_df %>%
+  inner_join(cluster_df) %>%
   inner_join(classification_data) %>%
   inner_join(pop_df) %>%
   group_by(code, location_name, population, income_group, cluster_no, cluster, measure_name, year) %>%
